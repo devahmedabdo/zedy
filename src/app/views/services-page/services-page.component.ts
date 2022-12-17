c1c054b61301394e48a826e24fd5173abfebb559
Ahmed Abdo <devahmedabdo@gmail.com>
2022-12-17 16:14:41 +0200

    add docs

--- src/app/views/services-page/services-page.component.ts
+++ src/app/views/services-page/services-page.component.ts
@@ -8,20 +8,5 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class ServicesPageComponent implements OnInit {
   constructor() {}
-  loading() {
-    setTimeout(() => {
-      document.querySelector('.loading')?.classList.remove('loading');
-      let animationClass = document.querySelectorAll(
-        '.lReveal , .dReveal,.uReveal,.rReveal'
-      );
-      window.onload = () => {
-        animationClass.forEach((e) => {
-          e.classList.add('reveal');
-        });
-      };
-    }, 100);
-  }
-  ngOnInit(): void {
-    this.loading();
-  }
+  ngOnInit(): void {}
 }
